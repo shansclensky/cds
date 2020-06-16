58ad7b07a12456dab92ff6d2a493e5a2265aebab
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2020-06-16 18:30:39 +0200

    fix: ui
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- ui/extra-webpack.config.js
+++ ui/extra-webpack.config.js
@@ -1,7 +1,15 @@
 let webpack = require('webpack');
+let path = require('path');
+
 
 module.exports = {
   plugins: [
     new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /fr|en/)
-  ]
+  ],
+  resolve: {
+    alias: {
+      "../../theme.config$": path.join(__dirname, "/semantic-ui/theme.config"),
+      "../semantic-ui/site": path.join(__dirname, "/semantic-ui/site")
+    }
+  }
 };
